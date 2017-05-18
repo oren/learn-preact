679647c874f0ab060bbec0e3cfd783885ba268e5
Oren <orengolan@gmail.com>
2017-05-17 22:34:46 -0700

    misc

--- style/src/components/home/index.js
+++ style/src/components/home/index.js
@@ -4,20 +4,36 @@ const classNames = require('classnames')
 
 export default class Profile extends Component {
 	render({user}) {
-		const tmp = classNames({
-      'foo': true,
-      'bar': true,
-    });
+		const tmp = classNames(
+      style.foo,
+      style.bar
+    )
+		console.log(tmp)
 
 		return (
 			<div>
-				<div className={tmp}>
+				<div class={tmp}>
 					test
 				</div>
-				<div className="foo bar">
-					test
+
+				<div class={classNames({
+					[style.foo]: true,
+					[style.bar]: true
+				})} >
+					foo
 				</div>
-				<div class={style.foo}>
+
+				<div class={classNames(
+					style.foo,
+					style.bar
+				)} >
+					foo
+				</div>
+
+				<div class={classNames({
+						[style.foo]: true,
+						[style.bar]: true,
+				})} >
 					test
 				</div>
 			</div>
